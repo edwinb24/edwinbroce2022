9fa636aad1c902f9e67983982fad9542d3ba8256
Edwin <edwin_b24@hotmail.com>
2022-03-30 17:49:32 -0500

    working on main menu and refactoring

--- wp-content/themes/edwinbrocetheme/header.php
+++ wp-content/themes/edwinbrocetheme/header.php
@@ -99,9 +99,9 @@ if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])==$lastModified || $etagHeader
                     <a href="mailto:broceedwin@gmail.com">broceedwin@gmail.com</a>
                 </nav>
                 <nav class="social_media" aria-label="Social Media Navigation">
-                    <a href="https://github.com/edwinb24" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_2tvaKuTdI.png" alt="Go to My GitHub"></a>
-                    <a href="https://www.linkedin.com/in/edwin-broce/" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_TMqTNg4N8w.png" alt="Go to My LinkedIn"></a>
-                    <a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My Youtube"></a>
+                    <a href="https://github.com/edwinb24" target="_blank" rel="nofollow noreferrer"><img class="github_image" src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_2tvaKuTdI.png" alt="Go to My GitHub"></a>
+                    <a href="https://www.linkedin.com/in/edwin-broce/" target="_blank" rel="nofollow noreferrer"><img class="linkedin_image" src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_TMqTNg4N8w.png" alt="Go to My LinkedIn"></a>
+                    <a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/" target="_blank" rel="nofollow noreferrer"><img class="youtube_image" src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My Youtube"></a>
                 </nav>
             </div>
         </div>
