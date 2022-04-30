9b994aa1046ff59e620005b057170dc299bb5e28
Edwin <edwin_b24@hotmail.com>
2022-04-30 15:35:37 -0500

    Change content, testing 3d effect and distances

--- wp-content/themes/edwinbrocetheme/front-page.php
+++ wp-content/themes/edwinbrocetheme/front-page.php
@@ -4,16 +4,89 @@
 <div class="scene_3d_wrapper">
 <div class="scene_3d_container">
 <div class="main_content_wrapper floating_elements_list">
-	<!-- <div class="floating_element">
-		<h1 class="entry-title">Front-End WordPress Developer at Colibri Group – 08/2017 to 07/2019</h1>				
-	</div> -->
-	<div class="floating_element">
-		<h1 class="entry-title">Front-End WordPress Developer at Colibri Group – 08/2017 to 07/2019</h1>				
-		<ul><li>Personally responsible for overhauling the company’s main product webpage, a change that is estimated to increase revenue by $1.5
-			 million yearly </li><li>Built a website to convert the company’s biggest platform from a transaction model to a subscription model,
-				  which allows the company to convert 32% of users to subscribers and increases average order value (AOV) from $12 to $34 </li>
-				  <li>Worked entirely remotely as part of a diverse, international team</li></ul>
+	<!-- Me -->
+	<div class="floating_element title_element"  id="me">
+		<h1 class="entry-title">Edwin Broce</h1>				
 	</div>
+	<div class="floating_element title_element">
+		<h1 class="entry-title">Full-Stack Web Developer</h1>				
+	</div>
+	<div class="floating_element title_element">
+		<h1 class="entry-title">Based on St.Louis, USA</h1>				
+	</div>
+	<div class="floating_element floating_paragraph">
+		<ul>
+			<li class="entry-title">Software Developer from Panama</li>				
+			<li class="entry-title">Done everything Full-Stack from database enginnering to Front-end Web Analytics</li>				
+			<li class="entry-title">
+				Focus on Designs that are:
+				<ul>Creative</ul>
+				<ul>Fast</ul>
+				<ul>Secure</ul>
+				<ul>Interactive</ul>
+			</li>				
+		</ul>
+	</div>
+
+	<!-- Work -->
+	<h1 class="floating_element section_title">WORK</h1>				
+	<div class="floating_element floating_element_with_background work">
+			<h1 class="entry-title">REIBlackBook</h1>
+			<p>
+				Lorem ipsum, or lipsum as it is sometimes known, is dummy 
+				text used in laying out print, graphic or web designs. The 
+				passage is attributed to an unknown typesetter in the 15th
+				 century who is thought to have scrambled parts of Cicero's De
+				 Finibus Bonorum et Malorum for use in a type specimen book. It 
+				 usually begins with:
+			</p>						
+	</div>
+	<div class="floating_element floating_element_with_background work">
+			<h1 class="entry-title">Colibri Group</h1>
+			<p>
+				Lorem ipsum, or lipsum as it is sometimes known, is dummy 
+				text used in laying out print, graphic or web designs. The 
+				passage is attributed to an unknown typesetter in the 15th
+				 century who is thought to have scrambled parts of Cicero's De
+				 Finibus Bonorum et Malorum for use in a type specimen book. It 
+				 usually begins with:
+			</p>						
+	</div>
+	<div class="floating_element floating_element_with_background work">
+			<h1 class="entry-title">Lumeris</h1>
+			<p>
+				Lorem ipsum, or lipsum as it is sometimes known, is dummy 
+				text used in laying out print, graphic or web designs. The 
+				passage is attributed to an unknown typesetter in the 15th
+				 century who is thought to have scrambled parts of Cicero's De
+				 Finibus Bonorum et Malorum for use in a type specimen book. It 
+				 usually begins with:
+			</p>						
+	</div>
+	<div class="floating_element floating_element_with_background work">
+			<h1 class="entry-title">APA</h1>
+			<p>
+				Lorem ipsum, or lipsum as it is sometimes known, is dummy 
+				text used in laying out print, graphic or web designs. The 
+				passage is attributed to an unknown typesetter in the 15th
+				 century who is thought to have scrambled parts of Cicero's De
+				 Finibus Bonorum et Malorum for use in a type specimen book. It 
+				 usually begins with:
+			</p>						
+	</div>
+	<div class="floating_element work">
+			<h1 class="entry-title"><i>Other Professional Stuff</i></h1>
+			<ul>
+				<li class="entry-title">MBA</li>
+				<li class="entry-title">Front-End Designer for 2 Years - MissTerios Boutique</li>
+				<li class="entry-title">Part-Time Realtor for 4 Years</li>				
+				<li class="entry-title">Sales Cleric - NY&C</li>				
+			</ul>						
+	</div>
+	<div class="floating_element outskirt_element">
+		<h1 class="entry-title">Based on St.Louis, USA</h1>				
+	</div>
+
 </div>
 </div>
 </div>
