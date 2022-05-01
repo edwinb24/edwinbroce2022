c2a315c6b425fc666d985f1fb52ecfc8ba828f1b
Edwin <edwin_b24@hotmail.com>
2022-05-01 12:33:25 -0500

    Working on css for elements on screen

--- wp-content/themes/edwinbrocetheme/front-page.php
+++ wp-content/themes/edwinbrocetheme/front-page.php
@@ -7,13 +7,10 @@
 	<!-- Me -->
 	<div class="floating_element title_element"  id="me">
 		<h1 class="entry-title">Edwin Broce</h1>				
-	</div>
-	<div class="floating_element title_element">
 		<h1 class="entry-title">Full-Stack Web Developer</h1>				
-	</div>
-	<div class="floating_element title_element">
 		<h1 class="entry-title">Based on St.Louis, USA</h1>				
 	</div>
+	<div class="floating_element floating_element_with_background work" id="myPicture"></div>
 	<div class="floating_element floating_paragraph">
 		<ul>
 			<li class="entry-title">Software Developer from Panama</li>				
@@ -30,17 +27,6 @@
 
 	<!-- Work -->
 	<h1 class="floating_element section_title">WORK</h1>				
-	<div class="floating_element floating_element_with_background work">
-			<h1 class="entry-title">REIBlackBook</h1>
-			<p>
-				Lorem ipsum, or lipsum as it is sometimes known, is dummy 
-				text used in laying out print, graphic or web designs. The 
-				passage is attributed to an unknown typesetter in the 15th
-				 century who is thought to have scrambled parts of Cicero's De
-				 Finibus Bonorum et Malorum for use in a type specimen book. It 
-				 usually begins with:
-			</p>						
-	</div>
 	<div class="floating_element floating_element_with_background work">
 			<h1 class="entry-title">Colibri Group</h1>
 			<p>
